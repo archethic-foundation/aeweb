44a497b373fc128adb0c24c29236c21fa03e34ce
redDwarf03 <sylvain@archethic.net>
2024-03-20 22:43:06 +0100

    💄 Update header

--- lib/ui/views/main_screen/layouts/header.dart
+++ lib/ui/views/main_screen/layouts/header.dart
@@ -1,6 +1,8 @@
 /// SPDX-License-Identifier: AGPL-3.0-or-later
-import 'package:aeweb/ui/themes/aeweb_theme_base.dart';
+import 'package:archethic_dapp_framework_flutter/archethic_dapp_framework_flutter.dart'
+    as aedappfm;
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Header extends StatelessWidget {
   const Header({
@@ -14,30 +16,42 @@ class Header extends StatelessWidget {
         const SizedBox(
           width: 16,
         ),
-        Image.asset(
-          'assets/images/logo_crystal.png',
-          height: 50,
-        ),
-        const SizedBox(
-          width: 8,
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 5),
-          child: Text(
-            'aeHosting',
-            style: TextStyle(
-              fontSize: 30,
-              color: ArchethicThemeBase.blue200,
+        if (aedappfm.Responsive.isMobile(context) == false)
+          SvgPicture.asset(
+            'assets/images/AELogo.svg',
+            height: 34,
+          )
+        else
+          Padding(
+            padding: const EdgeInsets.only(left: 5),
+            child: SvgPicture.asset(
+              'assets/images/AELogo.svg',
+              height: 24,
             ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 5, bottom: 20),
-          child: Text(
-            'Beta',
-            style: Theme.of(context).textTheme.labelMedium,
+        if (aedappfm.Responsive.isMobile(context) == false)
+          const SizedBox(
+            width: 8,
           ),
-        ),
+        if (aedappfm.Responsive.isMobile(context) == false)
+          Padding(
+            padding: const EdgeInsets.only(bottom: 4),
+            child: SelectableText(
+              'aeHosting',
+              style: TextStyle(
+                fontSize: 33,
+                color: aedappfm.ArchethicThemeBase.neutral0,
+              ),
+            ),
+          ),
+        if (aedappfm.Responsive.isMobile(context) == false)
+          Padding(
+            padding: const EdgeInsets.only(left: 5, bottom: 26),
+            child: SelectableText(
+              'Beta',
+              style: Theme.of(context).textTheme.labelMedium,
+            ),
+          ),
       ],
     );
   }
